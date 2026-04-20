e83777bd0d9993131ad26e7f7ccd93a9e4a00e81
M-Dunnet <133737285+M-Dunnet@users.noreply.github.com>
2026-04-21 10:51:36 +1200

    Refactor script to use argparse for file inputs

--- bin/Add_transctiptID_to_CDS_fasta.py
+++ bin/Add_transctiptID_to_CDS_fasta.py
@@ -1,35 +1,60 @@
-canonical_transcripts_bed = '/Users/dunmi18p/Documents/Panel_Checking/ExonLocations414/Coding_exons_Canoinical_Transcipts.bed'
-bed_file_path = '/Users/dunmi18p/Desktop/intermedate_files/Transcript_CCDS_ID.bed'
-fasta_file_path = '/Users/dunmi18p/Desktop/intermedate_files/Transcript_CDS_sequence.fasta'
-output_fasta_path = '/Users/dunmi18p/Desktop/Final_CDS_sequences.fasta'
+#!/usr/bin/env python3
 
-# Create a dictionary to store mappings between CCDS identifiers and bed file entries
+import argparse
+
+# ----------------------
+# Argument parsing
+# ----------------------
+parser = argparse.ArgumentParser(
+    description="Append gene IDs to FASTA headers using CCDS mappings from BED files."
+)
+
+parser.add_argument("--canonical_bed", required=True, help="BED file of canonical transcripts")
+parser.add_argument("--ccds_bed", required=True, help="BED file containing CCDS mappings")
+parser.add_argument("--input_fasta", required=True, help="Input FASTA file")
+parser.add_argument("--output_fasta", required=True, help="Output FASTA file")
+
+args = parser.parse_args()
+
+# ----------------------
+# Load canonical transcripts
+# ----------------------
+canonical_transcripts = {
+    line.strip().split('\t')[4]
+    for line in open(args.canonical_bed)
+    if not line.startswith('#')
+}
+
+# ----------------------
+# Build CCDS → transcript mapping
+# ----------------------
 ccds_mapping = {}
 
-# Store only canonical transcripts
-canonical_transcripts = {line.strip().split('\t')[4] for line in open(canonical_transcripts_bed) if not line.startswith('#')}
-print(canonical_transcripts)
-
-# Read the bed file and store mappings
-with open(bed_file_path, 'r') as bed_file:
+with open(args.ccds_bed, 'r') as bed_file:
     for line in bed_file:
-        if not line.startswith('#'):
-            fields = line.strip().split('\t')
-            transcript_id = fields[0]  # Extract the transcript ID
-            ccds_identifier = fields[5]  # Extract the CCDS identifier
-            if transcript_id not in canonical_transcripts:
-                continue
-            else:
-                ccds_mapping[ccds_identifier] = transcript_id  # Map CCDS identifier to transcript ID
+        if line.startswith('#'):
+            continue
 
-# Modify the fasta file with bed file information
-with open(fasta_file_path, 'r') as fasta_file, open(output_fasta_path, 'w') as output_fasta:
+        fields = line.strip().split('\t')
+        transcript_id = fields[0]
+        ccds_identifier = fields[5]
+
+        if transcript_id in canonical_transcripts:
+            ccds_mapping[ccds_identifier] = transcript_id
+
+# ----------------------
+# Update FASTA headers
+# ----------------------
+with open(args.input_fasta, 'r') as fasta_file, open(args.output_fasta, 'w') as output_fasta:
     for line in fasta_file:
         if line.startswith('>'):
             fasta_heading = line.strip()
-            ccds_identifier = fasta_heading.split(' ')[0].split('_')[2]  # Extract the CCDS identifier
-            bed_entry = ccds_mapping.get(ccds_identifier, '')  # Retrieve the corresponding bed file entry
-            new_fasta_heading = f'{fasta_heading} gene_ID={bed_entry}' if bed_entry else fasta_heading
-            output_fasta.write(new_fasta_heading + '\n')
+            ccds_identifier = fasta_heading.split(' ')[0].split('_')[2]
+
+            transcript_id = ccds_mapping.get(ccds_identifier)
+            if transcript_id:
+                fasta_heading = f"{fasta_heading} gene_ID={transcript_id}"
+
+            output_fasta.write(fasta_heading + '\n')
         else:
             output_fasta.write(line)
